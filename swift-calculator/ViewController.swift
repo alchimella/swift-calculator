cb3f456bb64d823dcd142eaed5d511e6405447e9
Abi Radzhabova <aradzhabova@askartec.com>
2021-11-04 11:38:29 +0600

    Initial Commit

---
+++ swift-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-calculator
+//
+//  Created by Abi  Radzhabova on 4/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
