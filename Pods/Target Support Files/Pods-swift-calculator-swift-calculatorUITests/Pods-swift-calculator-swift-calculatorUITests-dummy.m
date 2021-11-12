cd31b1eb87b9886a29f5b4781de05e1eaae3539a
Abi Radzhabova <aradzhabova@askartec.com>
2021-11-12 17:12:06 +0600

    Removed excess files

--- Pods/Target Support Files/Pods-swift-calculator-swift-calculatorUITests/Pods-swift-calculator-swift-calculatorUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_calculator_swift_calculatorUITests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_calculator_swift_calculatorUITests
-@end
--- Pods/Target Support Files/Pods-swift-calculatorTests/Pods-swift-calculatorTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_swift_calculatorTests : NSObject
-@end
-@implementation PodsDummy_Pods_swift_calculatorTests
-@end
